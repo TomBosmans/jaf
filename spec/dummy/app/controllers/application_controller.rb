1431c2f0716ad5b71a04ae87bd4e3a4e6b96873c
Tom Bosmans <tom.bosmans@efeso.com>
2020-03-03 11:43:47 +0100

    spec resources.rb

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -8,4 +8,21 @@ class ApplicationController < ActionController::API
   def serialize(resource, options = {})
     serializer.new(resource, options).serialized_json
   end
+
+  def create_resource(new_resource, attributes)
+    new_resource.attributes = attributes
+    new_resource.save
+    new_resource
+  end
+
+  def update_resource(resource, attributes)
+    resource.attributes = attributes
+    resource.save
+    resource
+  end
+
+  def destroy_resource(resource)
+    resource.destroy
+    resource
+  end
 end
--- spec/dummy/app/controllers/users_controller.rb
+++ spec/dummy/app/controllers/users_controller.rb
@@ -1,3 +1,9 @@
 class UsersController < ApplicationController
   include Jaf::Resources
+
+  private
+
+  def resource_params
+    attributes.permit(:name, :email)
+  end
 end
